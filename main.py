4f7e2f8c6c40fece229e09213e9557e9b45e696e
Pramod Kondur <pramodkondur@Pramods-MacBook-Air.local>
2024-07-12 17:52:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,227 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from datetime import datetime
+import pandas as pd
+import mysql.connector
+# use pip install webdriver-manager which is needed
+
+# Setup MYSQL connection
+con = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="expert789",
+    database="RedBus"
+)
+
+cursor = con.cursor()
+
+# Query to Create Table
+query = """CREATE TABLE if not exists bus_routes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    route_name TEXT,
+    route_link TEXT,
+    busname TEXT,
+    bustype TEXT,
+    departing_time DATETIME,
+    duration TEXT,
+    reaching_time DATETIME,
+    star_rating FLOAT,
+    price DECIMAL(10, 2),
+    seats_available INT
+    )"""
+cursor.execute(query)
+
+# Truncate Table
+query = "TRUNCATE TABLE bus_routes"
+cursor.execute(query)
+
+
+# Function to scroll to end of page to get all buses
+def scroll():
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # Scroll down to the bottom
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait for page to load
+
+        time.sleep(.2)
+
+        # Calculate new scroll height and compare with last scroll height
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+
+# Function to get bus details
+def get_bus_details_for_route(url):
+    driver.get(url)
+    route_info = (driver.find_element(By.CLASS_NAME, 'D136_h1').text).replace(' Bus', '')
+
+    dayelem = driver.find_element(By.XPATH, '//*[@id="searchDat"]')
+    dayvar = (dayelem.get_attribute('value')) + " 2024"
+
+    # waiting for page to load
+    time.sleep(4)
+
+    # Expanding all the buses section if it has 'view buses' button
+    try:
+        tourismBusesAgency = driver.find_elements(By.CLASS_NAME, 'gmeta-data.clearfix')
+
+        for agency in tourismBusesAgency:
+            btn_var = agency.find_element(By.CLASS_NAME, "button")
+            btn_var.click()
+    except:
+        print(url,'view-buses not clickable or not available')
+    # Scrolling so all the bus data gets loaded
+    scroll()
+
+    # Creating empty list to store the details of the buses
+    buses_list = []
+
+    # Getting all the buses group so that we can get all the bus details for each of them i.e. states and private buses
+    busesgroup = driver.find_elements(By.CLASS_NAME, 'bus-items')
+
+    for buses in busesgroup:
+        buseslist = buses.find_elements(By.CLASS_NAME, 'clearfix.bus-item')
+
+        #print("buses number:", len(buseslist))
+
+        # Gets all the required bus details in the selected group
+
+        for bus in buseslist:
+            busname = bus.find_element(By.CLASS_NAME, 'travels.lh-24.f-bold.d-color').text
+            # print(busname)
+            bustype = bus.find_element(By.CLASS_NAME, 'bus-type.f-12.m-top-16.l-color.evBus').text
+            departing_time = dayvar + " " + bus.find_element(By.CLASS_NAME, 'dp-time.f-19.d-color.f-bold').text + ":00"
+            duration = bus.find_element(By.CLASS_NAME, 'dur.l-color.lh-24').text
+
+            # Some buses have reaching date as the next day, so we are using try and exception to get these values
+            try:
+                bus.find_element(By.CLASS_NAME, 'next-day-dp-lbl.m-top-16') != 0
+                new_date = ((bus.find_element(By.CLASS_NAME, 'next-day-dp-lbl.m-top-16').text).replace("-",
+                                                                                                       " ")) + " 2024"
+                reaching_time = new_date + " " + bus.find_element(By.CLASS_NAME,
+                                                                  'bp-time.f-19.d-color.disp-Inline').text + ":00"
+
+            except NoSuchElementException:
+                reaching_time = dayvar + " " + bus.find_element(By.CLASS_NAME,
+                                                                'bp-time.f-19.d-color.disp-Inline').text + ":00"
+
+            star_rating = (((bus.find_element(By.CLASS_NAME, 'column-six.p-right-10.w-10.fl').text).replace("New",
+                                                                                                            "0")).replace(
+                " ", "0"))[0:3]
+            price = (bus.find_element(By.CLASS_NAME, 'fare.d-block').text).replace("INR ", "")
+            seats_available = (bus.find_element(By.CLASS_NAME, 'column-eight.w-15.fl').text)[0:2]
+
+            # print(bus_name, bus_type, departing_time, duration, reaching_time, star_rating, price, seats_available)
+
+            bus_item = {
+                'route_name': route_info,
+                'route_link': url,
+                'busname': busname,
+                'bustype': bustype,
+                'departing_time': datetime.strptime(departing_time, "%d %b %Y %H:%M:%S"),
+                'duration': duration,
+                'reaching_time': datetime.strptime(reaching_time, "%d %b %Y %H:%M:%S"),
+                'star_rating': float(star_rating),
+                'price': float(price),
+                'seats_available': float(seats_available[0:2])
+
+            }
+
+            buses_list.append(bus_item)
+
+    # Once all details of the buses are got for a route, we convert it to a dataframe so that we can write it to the db
+    df = pd.DataFrame(buses_list)
+    return (df)
+
+
+# Function to write the scrapped data into database
+def write_into_db(df):
+    query = """insert into bus_routes (route_name,route_link,busname,bustype,departing_time,duration,reaching_time,star_rating,price,seats_available) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+             """
+    result = []
+
+    for index in df.index:
+        row_data = list(df.loc[index].values)
+        result.append(row_data)
+    cursor.executemany(query,
+                       result)  # execute many and storing data in list as it connects to the db once it finishes getting input rather than each time
+    con.commit()
+
+
+# A list to store all the route urls for each state transportation
+routes_urls = []
+
+
+# Function to get all the routes url for the selected state transportation
+def get_urls(url):
+    driver.get(url)
+    time.sleep(2)
+
+    routes_list_to_get_urls = driver.find_elements(By.CLASS_NAME, 'route_link')
+    # This line used for checking no.of routes fetched from the page --> print(len(routes_list_to_get_urls))
+    # Gets all routes for page 1
+    for routes in routes_list_to_get_urls:
+        url_to_extract = routes.find_element(By.TAG_NAME, 'a')
+        url_extracted = url_to_extract.get_attribute('href')
+        #print(url_extracted)
+        routes_urls.append(url_extracted)
+
+    # To handle pagination
+    page_elements = driver.find_elements(By.CLASS_NAME, 'DC_117_pageTabs')
+
+    for page in page_elements:
+        try:
+            page.click()
+            print(page.text)
+            time.sleep(.2)
+            routes_list_to_get_urls = driver.find_elements(By.CLASS_NAME, 'route_link')
+            # This line used for checking no.of routes fetched from the page --> print(len(routes_list_to_get_urls))
+            # Gets all routes for page 2 to end
+            for routes in routes_list_to_get_urls:
+                url_to_extract = routes.find_element(By.TAG_NAME, 'a')
+                url_extracted = url_to_extract.get_attribute('href')
+                #print(url_extracted)
+                routes_urls.append(url_extracted)
+        except:
+            print('not clickable')
+
+
+list_of_state_tourism_url = ['https://www.redbus.in/online-booking/apsrtc/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/ksrtc-kerala/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/astc/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/pepsu/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/ktcl/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/wbtc-ctc/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/jksrtc',
+                             'https://www.redbus.in/online-booking/tsrtc/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/rsrtc/?utm_source=rtchometile',
+                             'https://www.redbus.in/online-booking/hrtc/?utm_source=rtchometile'
+                             ]
+
+# Collecting all the urls for routes from the state tourism urls
+for url in list_of_state_tourism_url:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    get_urls(url)
+
+# Collects all the bus details data from each route url we collected and writes to db
+for url in routes_urls:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    df = get_bus_details_for_route(url)
+    write_into_db(df)
+
+con.close()
---
+++ streamlit.py
@@ -0,0 +1,112 @@
+import mysql.connector
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+
+# Connect to MySQL database
+con = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="expert789",
+    database="RedBus"
+)
+cursor = con.cursor(dictionary=True)
+
+# Execute initial query to fetch data from MySQL
+cursor.execute("SELECT * FROM bus_routes")
+data = cursor.fetchall()
+
+# Close the cursor after fetching data
+cursor.close()
+
+# Convert data to Pandas DataFrame
+df = pd.DataFrame(data)
+
+# Streamlit UI Title
+st.title('RedBus Buses')
+
+
+# Sidebar filters
+st.sidebar.title('Filters')
+
+# Unique route names
+route_names = df['route_name'].unique().tolist()
+route_name = st.sidebar.selectbox('Select Route Name', ['', *route_names])
+
+# Bus types
+bus_types = ['AC', 'Non AC', 'Others']
+bus_type = st.sidebar.selectbox('Select Bus Type', ['', *bus_types])
+
+# Seater types
+seat_types = ['Sleeper', 'Semi Sleeper', 'Seater', 'Others']
+seat_type = st.sidebar.selectbox('Select Seat Type', ['', *seat_types])
+
+# Hours of departure
+bus_hours_disp = {}
+for i in range(24):
+    if i != 23:
+        bus_hours_disp[i] = f"{i}:00 to {i+1}:00"
+    else:
+        bus_hours_disp[i] = f"{i}:00 to 0:00"
+
+bus_hours = list(range(24))
+
+departing_time_options = [''] + bus_hours
+
+departing_time = st.sidebar.selectbox("Starting time hour",departing_time_options, format_func=lambda x: bus_hours_disp[x] if x in bus_hours_disp else '')
+
+# Minimum star rating slider
+min_rating = st.sidebar.slider('Minimum Star Rating', min_value=0.0, max_value=5.0, step=0.1, value=0.0)
+
+# Maximum ticket price
+max_price = st.sidebar.number_input('Maximum Ticket Price', min_value=0.0, format="%.2f", value=10000.0)
+
+
+# SQL queries for filtering data
+query = "SELECT * FROM bus_routes WHERE 1=1"
+
+# Apply filters based on user selections
+if route_name:
+    query += f" AND route_name = '{route_name}'"
+
+if bus_type == 'AC':
+    query += " AND (bustype REGEXP 'AC|A.C|A/C') AND (bustype NOT REGEXP 'Non AC|Non A/C|Non A.C|NON-AC')"
+elif bus_type == 'Non AC':
+    query += " AND (bustype REGEXP 'Non AC|Non A/C|Non A.C|NON-AC')"
+elif bus_type == 'Others':
+    query += " AND (bustype NOT REGEXP 'AC|A.C|A/C|Non AC|Non A/C|Non A.C|NON-AC') AND (bustype IS NOT NULL)"
+
+if seat_type:
+    if seat_type == 'Sleeper':
+        query += " AND bustype LIKE '%Sleeper%' AND bustype NOT LIKE '%Semi Sleeper%'"
+    elif seat_type == 'Semi Sleeper':
+        query += " AND bustype LIKE '%Semi Sleeper%'"
+    elif seat_type == 'Seater':
+        query += " AND bustype LIKE '%Seater%'"
+    elif seat_type == 'Others':
+        query += " AND (bustype NOT LIKE '%Sleeper%' AND bustype NOT LIKE '%Semi Sleeper%' AND bustype NOT LIKE '%Seater%' AND bustype IS NOT NULL)"
+
+if departing_time != '':
+    if departing_time or departing_time == 0:
+        query += f" AND HOUR(departing_time) = {(departing_time)}"
+
+query += f" AND star_rating >= {min_rating} AND price <= {max_price}"
+
+# Execute final query
+cursor = con.cursor(dictionary=True)
+cursor.execute(query)
+filtered_data = cursor.fetchall()
+cursor.close()
+
+# Convert filtered data to DataFrame to display in streamlit
+filtered_df = pd.DataFrame(filtered_data)
+
+# Display the filtered data in a table
+if not filtered_df.empty:
+    st.write("Total Results:", len(filtered_df))
+    st.dataframe(filtered_df.drop(columns=['route_link']))
+else:
+    st.warning('No bus routes found with the specified filters.')
+
+# Close MySQL connection
+con.close()
